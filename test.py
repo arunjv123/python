405be600f485ebc85543d24c4e07e8d1cbc0fabf
arunjv123 <37876334+arunjv123@users.noreply.github.com>
2018-03-28 21:26:56 +0530

    age criteria added

--- test.py
+++ test.py
@@ -4,3 +4,10 @@ for x in b:
 	print x,
 c = reduce(lambda x,y:x+y, a)
 print c
+#this is a comment
+name = raw_input("enter you name: ")
+age = raw_input("enter you age: ")
+if age > 18:
+	print ("you are elligible to vote!", name)
+else:
+	print ("sorry. You can't vote now !", name)
